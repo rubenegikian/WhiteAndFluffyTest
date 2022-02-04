46a72ba6aaa124e773971ad015c1afb03774324d
Ruben Egikian <rubenegikian@gmail.com>
2022-02-04 14:33:41 +0300

    Initial Commit

---
+++ WhiteAndFluffyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiteAndFluffyTest
+//
+//  Created by Ruben Egikian on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
